d2fb8deb4534f5c570f3e1143da4096ed0684e81
Nicholas1126 <282235411@qq.com>
2017-08-05 19:38:05 +0800

    Add CONFIG_KASAN feature for fuzzing

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,5 @@
 make ARCH=arm64 distclean
 rm -rf ../out
 mkdir ../out
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=../out merge_hi3660_defconfig > build.log 2>&1
-cat rules.txt >> ../out/.config
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=../out fuzz_hi3360_defconfig > build.log 2>&1
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=../out -j8 >> build.log 2>&1
